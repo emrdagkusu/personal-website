513582ad2c58efcedf40d0b58302f99205af4d70
emrdagkusu <emrdagkusu@gmail.com>
2022-02-17 23:38:19 +0300

    update not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
 import React from "react"
 
+import Layout from "../components/Layout"
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <Layout>
+      <div style={{ textAlign: `center` }}>
+        <h1>404</h1>
+        <p>Page not found</p>
+        <a href="/" style={{ color: `black` }}>
+          Go to home page
+        </a>
+      </div>
+    </Layout>
   )
 }
 
