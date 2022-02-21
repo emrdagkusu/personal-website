37603b8089a1ccc5d18cd5706c8fc005182c0ffe
emrdagkusu <emrdagkusu@gmail.com>
2022-02-21 20:21:08 +0300

    add download cv button

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
+import CV from "../../../static/Emrullah_Dagkusu_CV.pdf";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear() + " "} Emrullah Dağkuşu</p>
-            <a href="mailto:emrdagkusu@gmail.com" style={{ color: `black` }}>
-                emrdagkusu@gmail.com
-            </a>
+            <div style={{marginTop: `auto`}}>
+                <a href={CV} download="Emrullah_Dagkusu_CV" style={{ color: `black` }}>Download CV</a>
+            </div>
+            <div style={{textAlign: `end`}}>
+                <p>© {new Date().getFullYear() + " "} Emrullah Dağkuşu</p>
+                <a href="mailto:emrdagkusu@gmail.com" style={{ color: `black` }}>
+                    emrdagkusu@gmail.com
+                </a>
+            </div>
+            
         </footer>
     )
 }
