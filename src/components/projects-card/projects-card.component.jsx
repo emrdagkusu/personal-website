f269575970a104761c6a0b97dd90159d708e0b7b
emrdagkusu <emrdagkusu@gmail.com>
2022-02-21 20:20:42 +0300

    add new project

--- src/components/projects-card/projects-card.component.jsx
+++ src/components/projects-card/projects-card.component.jsx
@@ -8,23 +8,22 @@ const ProjectsCard = () => {
         <div className="main-container">
             <h1 className="section-title">Projects</h1>
             <ProjectContainer>
-                <a href="https://github.com/emrdagkusu/Auto-Grading-System" 
+                <a href="https://github.com/hbusul/kucukdev_api" 
                 target="_blank" rel="noreferrer" style={{color: `black`}}>
-                    <h1 className="title">Auto Grading System</h1>
+                    <h1 className="title">Kucukdev</h1>
                 </a>
                 <h1 className="secondary-title" style={{color: `#d32f2f`}}>
-                    Auto grading system for universities with online code editor
+                    Assistant for university students
                 </h1>
                 <ul>
                     <li className="list-item">
                         <div className="skill-row">
-                            Technologies: Docker, Python, FastAPI, React.js, Tailwind.css, Judge0 API
+                            Technologies: Docker, Python FastAPI, Pytest, Uvicorn, React.js
                         </div>
                     </li>
                     <li className="list-item">
-                        Show students, courses, assignments.
-                        Add courses, students, assignments, test cases if you are a professor.
-                        Solve assignments and see auto calculated grades right after if you are a student.
+                        Track attendance, pick lessons in a way that they don't conflict. 
+                        See schedule and overview of the week.
                     </li>
                 </ul>
             </ProjectContainer>
@@ -45,7 +44,26 @@ const ProjectsCard = () => {
                     <li className="list-item">
                         Get notiﬁcation about your GitHub account. 
                         Create a list with favorite users and notify about their changes at GitHub.
-                        Save ideas in the note part of the proﬁle.
+                    </li>
+                </ul>
+            </ProjectContainer>
+            <ProjectContainer>
+                <a href="https://github.com/emrdagkusu/Auto-Grading-System" 
+                target="_blank" rel="noreferrer" style={{color: `black`}}>
+                    <h1 className="title">Auto Grading System</h1>
+                </a>
+                <h1 className="secondary-title" style={{color: `#d32f2f`}}>
+                    Auto grading system for universities with online code editor
+                </h1>
+                <ul>
+                    <li className="list-item">
+                        <div className="skill-row">
+                            Technologies: Docker, Python, FastAPI, React.js, Tailwind.css, Judge0 API
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        Create assignments, test cases if professor. 
+                        Solve assignments and see auto calculated grades right after if student.
                     </li>
                 </ul>
             </ProjectContainer>
